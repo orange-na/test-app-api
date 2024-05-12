96fd05af05e77d5f463d582d9ab646d5a71ce5b7
orange-na <otuy1125abman@gmail.com>
2024-05-12 22:30:43 +0900

    test prisma client

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getMe() {
+    return this.prisma.user.findMany();
+  }
+}
